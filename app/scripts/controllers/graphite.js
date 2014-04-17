6c7c81b4e06907af9ca1937b716d53ecd157bdac
awashbrook <awashbrook@gmail.com>
2014-04-17 08:34:36 +0100

    Fixed type cut paster in this graphite dash

--- app/scripts/controllers/graphite.js
+++ app/scripts/controllers/graphite.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('app')
-  .controller('GraphiteCtrl', function ($scope, $interval, stackedAreaChartSampleData) {
+  .controller('GraphiteCtrl', function ($scope, $interval, stackedAreaChartSampleData, GraphiteTimeSeriesDataModel, RandomTopNDataModel) {
     var widgetDefinitions = [
       {
         name: 'nvd3-stacked-area-chart',
@@ -24,7 +24,7 @@ angular.module('app')
 //                 target="stats.amers.alpha-us1-cell.eed-erp-cprp.us1i-erpcprp01.os.cpu.usage"
 //                 ></graphite>
 //  
-     
+           
       {
         name: 'graphite',
         attrs: {
@@ -32,113 +32,40 @@ angular.module('app')
           from:'-6h',
           until: 'now',
           target:'stats.amers.alpha-us1-cell.eed-erp-cprp.us1i-erpcprp01.os.cpu.usage'
-        },
-        style: {
-          width: '50%'
-        }
-      }
-
-// TODO - WIP       //      
-      // {
-      //   name: 'graphite',
-      //   directive: 'graphite',
-      //   dataAttrName: 'graphite',
-      //   dataModelType: GraphiteTimeSeriesDataModel,
-      //   dataModelOptions: {
-      //     params: {
-      //       url: 'http://metrics.alpha.eikon-mon.int.thomsonreuters.com/render/',
-      //       from:'-6h',
-      //       until: 'now',
-      //       target:'stats.amers.alpha-us1-cell.eed-erp-cprp.us1i-erpcprp01.os.cpu.usage'
-      //     }
-      //   },
-      //   style: {
-      //     width: '50%'
-      //   }
-      // }
-      
-    ];
-
-    var defaultWidgets = _.map(widgetDefinitions, function (widgetDef) {
-      return {
-        name: widgetDef.name
-      };
-    });
-
-    $scope.dashboardOptions = {
-      //AW Breaks my app...needs troubleshooting
-      // useLocalStorage: true, 
-      widgetButtons: true,
-      widgetDefinitions: widgetDefinitions,
-      defaultWidgets: defaultWidgets,
-      //AW Set custom widget template for graphite directive
-      optionsTemplateUrl: 'scripts/widgets/graphite/graphite-options.tpl.html'
-    };
-
-    // nvd3-stacked-area-chart
-    $scope.stackedAreaChartData = stackedAreaChartSampleData;
-
-    $scope.xAxisTickFormat = function () {
-      return function (d) {
-        return d3.time.format('%x')(new Date(d));
-      };
-    };
-
-    // external controls
-    $scope.addWidget = function (directive) {
-      $scope.dashboardOptions.addWidget({
-        name: directive
-      });
-    };
-
-    $scope.addWidgetScopeWatch = function () {
-      $scope.dashboardOptions.addWidget({
-        name: 'scope-watch',
-        attrs: {
-          value: 'randomValue'
-        }
-      });
-    };
-  });'use strict';
-
-angular.module('app')
-  .controller('GraphiteCtrl', function ($scope, $interval, stackedAreaChartSampleData) {
-    var widgetDefinitions = [
-      {
-        name: 'nvd3-stacked-area-chart',
-        attrs: {
-          data: 'stackedAreaChartData',
-          height: '400',
-          showXAxis: 'true',
-          showYAxis: 'true',
-          xAxisTickFormat: 'xAxisTickFormat()'
         },
         style: {
           width: '50%'
         }
       },
-//AW
-//                 <graphite url="http://metrics.alpha.eikon-mon.int.thomsonreuters.com/render/"
-//                 annotations="[]"
-//                 from="-6h"
-//                 until="now"
-//                 target="stats.amers.alpha-us1-cell.eed-erp-cprp.us1i-erpcprp01.os.cpu.usage"
-//                 ></graphite>
-//       
+
+      // TODO - WIP       //      
       {
-        name: 'graphite',
-        attrs: {
-          url: 'http://metrics.alpha.eikon-mon.int.thomsonreuters.com/render/',
-          from:'-6h',
-          until: 'now',
-          target:'stats.amers.alpha-us1-cell.eed-erp-cprp.us1i-erpcprp01.os.cpu.usage'
+        name: 'graphite2',
+        directive: 'graphite',
+        dataAttrName: 'graphite',
+        dataModelType: GraphiteTimeSeriesDataModel,
+        dataModelOptions: {
+          params: {
+            url: 'http://metrics.alpha.eikon-mon.int.thomsonreuters.com/render/',
+            from:'-6h',
+            until: 'now',
+            target:'stats.amers.alpha-us1-cell.eed-erp-cprp.us1i-erpcprp01.os.cpu.usage'
+          }
         },
         style: {
           width: '50%'
         }
+      },
+      
+      {
+        name: 'wt-top-n',
+        dataAttrName: 'data',
+        dataModelType: RandomTopNDataModel,
+        style: {
+          width: '30%'
+        }
       }
     ];
-
 
     var defaultWidgets = _.map(widgetDefinitions, function (widgetDef) {
       return {
@@ -147,7 +74,7 @@ angular.module('app')
     });
 
     $scope.dashboardOptions = {
-      //AW Breaks my app...needs troubleshooting
+      //AW TODO: Breaks my app...needs troubleshooting
       // useLocalStorage: true, 
       widgetButtons: true,
       widgetDefinitions: widgetDefinitions,
