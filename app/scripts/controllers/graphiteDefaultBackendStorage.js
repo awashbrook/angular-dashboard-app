1a349de8daf4d67b74dab0408218fee9c7e0a48e
awashbrook <awashbrook@gmail.com>
2014-05-14 12:21:27 +0100

    Tests passing for ES using elasticStorage web storage polyfill

--- app/scripts/controllers/graphiteDefaultBackendStorage.js
+++ app/scripts/controllers/graphiteDefaultBackendStorage.js
@@ -2,7 +2,7 @@
 
 // Storage enabled for this...this will be what a fresh dashboard will look like for new team
 angular.module('app')
-  .controller('GraphiteDefaultBackendStorageCtrl', function ($scope, $interval, es, nvd3ChartDefAttrs, WidgetDefaults) {
+  .controller('GraphiteDefaultBackendStorageCtrl', function ($scope, $interval, es, elasticStorage, nvd3ChartDefAttrs, WidgetDefaults) {
 
     es.ping({
       // ping usually has a 100ms timeout
@@ -33,65 +33,67 @@ angular.module('app')
     // How does Grafana creat and read documents
 
 //    http://events.alpha.eikon-mon.int.thomsonreuters.com/api//grafana-dash/dashboard/Alpha%20WFWS
-    es.get({
-      index: 'grafana-dash',
-      type: 'dashboard',
-      id: 'Alpha WFWS',
-      requestTimeout: 2000
-    }).then(function (body) {
-      console.log(body);
-    }, function (error) {
-      console.error(error.message);
-    });
+//    es.get({
+//      index: 'grafana-dash',
+//      type: 'dashboard',
+//      id: 'Alpha WFWS',
+//      requestTimeout: 2000
+//    }).then(function (body) {
+//      console.log(body);
+//    }, function (error) {
+//      console.error(error.message);
+//    });
 
     var sampleGrafanaDashboard = {"title":"Alpha WFWS Copy 2","services":{"filter":{"list":[],"time":{"from":"now-6h","to":"now"}}},"rows":[{"title":"Row1","height":"250px","editable":true,"collapse":false,"collapsable":true,"panels":[{"span":8,"editable":true,"type":"graphite","loadingEditor":false,"datasource":null,"renderer":"flot","x-axis":true,"y-axis":true,"scale":1,"y_formats":["short","short"],"grid":{"max":null,"min":0,"threshold1":null,"threshold2":null,"threshold1Color":"rgba(216, 200, 27, 0.27)","threshold2Color":"rgba(234, 112, 112, 0.22)"},"annotate":{"enable":false},"resolution":100,"lines":true,"fill":1,"linewidth":2,"points":false,"pointradius":5,"bars":false,"stack":false,"legend":{"show":true,"values":false,"min":false,"max":false,"current":false,"total":false,"avg":false},"percentage":false,"zerofill":true,"nullPointMode":"connected","steppedLine":false,"tooltip":{"value_type":"cumulative","query_as_alias":true},"targets":[{"target":"stats.amers.alpha-us1-cell.iap-cpm-wfws.*.os.cpu.cpuusage"}],"aliasColors":{},"aliasYAxis":{},"title":"WFWS CPU"}],"notice":false},{"title":"","height":"150px","editable":true,"collapse":false,"collapsable":true,"panels":[],"notice":false}],"editable":true,"failover":false,"panel_hints":true,"style":"dark","pulldowns":[{"type":"filtering","collapse":false,"notice":false,"enable":false},{"type":"annotations","enable":false}],"nav":[{"type":"timepicker","collapse":false,"notice":false,"enable":true,"status":"Stable","time_options":["5m","15m","1h","6h","12h","24h","2d","7d","30d"],"refresh_intervals":["5s","10s","30s","1m","5m","15m","30m","1h","2h","1d"],"now":true}],"loader":{"save_gist":false,"save_elasticsearch":true,"save_local":true,"save_default":true,"save_temp":true,"save_temp_ttl_enable":true,"save_temp_ttl":"30d","load_gist":false,"load_elasticsearch":true,"load_elasticsearch_size":20,"load_local":false,"hide":false},"refresh":false,"tags":[],"timezone":"browser"};
 
     //    http://events.alpha.eikon-mon.int.thomsonreuters.com/api//grafana-dash/dashboard/Alpha%20WFWS
-    es.create({
-      index: 'grafana-dash',
-      type: 'dashboard',
-      id: 'Alpha WFWS Copy 2',
-      body: sampleGrafanaDashboard,
-      requestTimeout: 2000
-    }).then(function (body) {
-      console.log(body);
-    }, function (error) {
-      console.error(error.message);
-    });
+//    es.create({
+//      index: 'grafana-dash',
+//      type: 'dashboard',
+//      id: 'Alpha WFWS Copy 2',
+//      body: sampleGrafanaDashboard,
+//      requestTimeout: 2000
+//    }).then(function (body) {
+//      console.log(body);
+//    }, function (error) {
+//      console.error(error.message);
+//    });
 
     var index = 'malhar-dash';
     var type = 'dashboard';
-    var storageId = 'andy-dashboard-graphite-default-backend-storage';
+    var storageId = 'andy-dashboard-graphite-default-backend-storage3';
     var sampleMalharDashboard = {"widgets":[{"title":"Mat","name":"nvLineChartAlpha","style":{"width":"400px"},"dataModelOptions":{"params":{"url":"http://metrics.alpha.eikon-mon.int.thomsonreuters.com/render/","from":"-2h","until":"now","target":["randomWalk(\"random walk 1\")","randomWalk(\"random walk 2\")","randomWalk(\"random walk 3\")"]}},"attrs":{"isArea":true,"height":400,"showXAxis":true,"showYAxis":true,"xAxisTickFormat":"xAxisTickFormat()","interactive":true,"useInteractiveGuideline":true,"tooltips":true,"showLegend":true,"color":"colorFunction()","forcey":"[0,2]"}},{"title":"Jan","name":"nvLineChartBeta","style":{"width":"400px"},"dataModelOptions":{"params":{"url":"http://metrics.beta.eikon-mon.int.thomsonreuters.com/render/","from":"-2h","until":"now","target":["randomWalk(\"andy random walk 1\")","randomWalk(\"olivier random walk 2\")","randomWalk(\"fabrice random walk 3\")"]}},"attrs":{"isArea":true,"height":400,"showXAxis":true,"showYAxis":true,"xAxisTickFormat":"xAxisTickFormat()","interactive":true,"useInteractiveGuideline":true,"tooltips":true,"showLegend":true,"color":"colorFunction()","forcey":"[0,2]"}},{"title":"Widget 3","name":"nvLineChartProd","style":{"width":"400px"},"dataModelOptions":{"params":{"url":"http://metrics.eikon-mon.int.thomsonreuters.com/render/","from":"-2h","until":"now","target":["randomWalk(\"random walk 1\")","randomWalk(\"random walk 2\")","randomWalk(\"random walk 3\")"]}},"attrs":{"isArea":true,"height":400,"showXAxis":true,"showYAxis":true,"xAxisTickFormat":"xAxisTickFormat()","interactive":true,"useInteractiveGuideline":true,"tooltips":true,"showLegend":true,"color":"colorFunction()","forcey":"[0,2]"}}]};
 
-    es.create({
-      index: index,
-      type: type,
-      id: storageId,
-      body: sampleMalharDashboard,
-      requestTimeout: 2000
-    }).then(function (body) {
-      console.log(body);
-    }, function (error) {
-      console.error(error.message);
-    });
+//    es.create({
+//      index: index,
+//      type: type,
+//      id: storageId,
+//      body: sampleMalharDashboard,
+//      requestTimeout: 2000
+//    }).then(function (body) {
+//      console.log(body);
+//    }, function (error) {
+//      console.error(error.message);
+//    });
+//
+//    es.get({
+//      index: index,
+//      type: type,
+//      id: storageId,
+//      requestTimeout: 2000
+//    }).then(function (body) {
+//      console.log(body);
+//    }, function (error) {
+//      console.error(error.message);
+//    });
 
-    es.get({
-      index: index,
-      type: type,
-      id: storageId,
-      requestTimeout: 2000
-    }).then(function (body) {
-      console.log(body);
-    }, function (error) {
-      console.error(error.message);
-    });
+    elasticStorage.setItem(storageId, sampleMalharDashboard);
+    elasticStorage.getItem(storageId);
 
-    var esStorage = null; // TODO
 
 
     $scope.dashboardOptions = {
-//      storage: esStorage,
+      storage: elasticStorage,
       storageId: storageId,
       widgetButtons: true,
       widgetDefinitions: WidgetDefaults.widgetDefaultDefinitions,
--- app/scripts/services/es.js
+++ app/scripts/services/es.js
@@ -6,10 +6,49 @@
 
 angular.module('app.service')
   .service('es', function (esFactory) {
-  return esFactory({
-    host: 'http://events.alpha.eikon-mon.int.thomsonreuters.com/api/'  // EMS proxies via apache
+    return esFactory({
+      host: 'http://events.alpha.eikon-mon.int.thomsonreuters.com/api/'  // EMS proxies via apache
 
-    // http://www.elasticsearch.org/guide/en/elasticsearch/client/javascript-api/current/configuration.html#config-options
-  });
-});
+      // http://www.elasticsearch.org/guide/en/elasticsearch/client/javascript-api/current/configuration.html#config-options
+    })
+  })
+  .service('elasticStorage', function (es) {
+      function elasticStorage() {}
+
+      elasticStorage.index = 'malhar-dash';
+      elasticStorage.type = 'dashboard';
+//      elasticStorage.storageId = 'andy-dashboard-graphite-default-backend-storage';
+//      var sampleMalharDashboard = {"widgets":[{"title":"Mat","name":"nvLineChartAlpha","style":{"width":"400px"},"dataModelOptions":{"params":{"url":"http://metrics.alpha.eikon-mon.int.thomsonreuters.com/render/","from":"-2h","until":"now","target":["randomWalk(\"random walk 1\")","randomWalk(\"random walk 2\")","randomWalk(\"random walk 3\")"]}},"attrs":{"isArea":true,"height":400,"showXAxis":true,"showYAxis":true,"xAxisTickFormat":"xAxisTickFormat()","interactive":true,"useInteractiveGuideline":true,"tooltips":true,"showLegend":true,"color":"colorFunction()","forcey":"[0,2]"}},{"title":"Jan","name":"nvLineChartBeta","style":{"width":"400px"},"dataModelOptions":{"params":{"url":"http://metrics.beta.eikon-mon.int.thomsonreuters.com/render/","from":"-2h","until":"now","target":["randomWalk(\"andy random walk 1\")","randomWalk(\"olivier random walk 2\")","randomWalk(\"fabrice random walk 3\")"]}},"attrs":{"isArea":true,"height":400,"showXAxis":true,"showYAxis":true,"xAxisTickFormat":"xAxisTickFormat()","interactive":true,"useInteractiveGuideline":true,"tooltips":true,"showLegend":true,"color":"colorFunction()","forcey":"[0,2]"}},{"title":"Widget 3","name":"nvLineChartProd","style":{"width":"400px"},"dataModelOptions":{"params":{"url":"http://metrics.eikon-mon.int.thomsonreuters.com/render/","from":"-2h","until":"now","target":["randomWalk(\"random walk 1\")","randomWalk(\"random walk 2\")","randomWalk(\"random walk 3\")"]}},"attrs":{"isArea":true,"height":400,"showXAxis":true,"showYAxis":true,"xAxisTickFormat":"xAxisTickFormat()","interactive":true,"useInteractiveGuideline":true,"tooltips":true,"showLegend":true,"color":"colorFunction()","forcey":"[0,2]"}}]};
+
+      elasticStorage.getItem = function (key) {
+        es.get({
+          index: elasticStorage.index,
+          type: elasticStorage.type,
+          id: key,
+          requestTimeout: 2000
+        }).then(function (body) {
+          console.log(body);
+        }, function (error) {
+          console.error(error.message);
+        });
+      };
+      elasticStorage.setItem = function (key, value) {
+        es.create({
+          index: elasticStorage.index,
+          type: elasticStorage.type,
+          id: key,
+          body: value,
+          requestTimeout: 2000
+        }).then(function (body) {
+          console.log(body);
+        }, function (error) {
+          console.error(error.message);
+        });
+      };
+      elasticStorage.removeItem = function (key) {};
+
+
+      return elasticStorage;
+
+    })
 
