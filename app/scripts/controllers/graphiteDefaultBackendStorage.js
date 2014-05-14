ed50b106ef21756876a4ebe256cc1cd46408685d
awashbrook <awashbrook@gmail.com>
2014-05-14 11:57:20 +0100

    Tests Create /  Read Working for Malhar

--- app/scripts/controllers/graphiteDefaultBackendStorage.js
+++ app/scripts/controllers/graphiteDefaultBackendStorage.js
@@ -59,11 +59,40 @@ angular.module('app')
       console.error(error.message);
     });
 
+    var index = 'malhar-dash';
+    var type = 'dashboard';
+    var storageId = 'andy-dashboard-graphite-default-backend-storage';
+    var sampleMalharDashboard = {"widgets":[{"title":"Mat","name":"nvLineChartAlpha","style":{"width":"400px"},"dataModelOptions":{"params":{"url":"http://metrics.alpha.eikon-mon.int.thomsonreuters.com/render/","from":"-2h","until":"now","target":["randomWalk(\"random walk 1\")","randomWalk(\"random walk 2\")","randomWalk(\"random walk 3\")"]}},"attrs":{"isArea":true,"height":400,"showXAxis":true,"showYAxis":true,"xAxisTickFormat":"xAxisTickFormat()","interactive":true,"useInteractiveGuideline":true,"tooltips":true,"showLegend":true,"color":"colorFunction()","forcey":"[0,2]"}},{"title":"Jan","name":"nvLineChartBeta","style":{"width":"400px"},"dataModelOptions":{"params":{"url":"http://metrics.beta.eikon-mon.int.thomsonreuters.com/render/","from":"-2h","until":"now","target":["randomWalk(\"andy random walk 1\")","randomWalk(\"olivier random walk 2\")","randomWalk(\"fabrice random walk 3\")"]}},"attrs":{"isArea":true,"height":400,"showXAxis":true,"showYAxis":true,"xAxisTickFormat":"xAxisTickFormat()","interactive":true,"useInteractiveGuideline":true,"tooltips":true,"showLegend":true,"color":"colorFunction()","forcey":"[0,2]"}},{"title":"Widget 3","name":"nvLineChartProd","style":{"width":"400px"},"dataModelOptions":{"params":{"url":"http://metrics.eikon-mon.int.thomsonreuters.com/render/","from":"-2h","until":"now","target":["randomWalk(\"random walk 1\")","randomWalk(\"random walk 2\")","randomWalk(\"random walk 3\")"]}},"attrs":{"isArea":true,"height":400,"showXAxis":true,"showYAxis":true,"xAxisTickFormat":"xAxisTickFormat()","interactive":true,"useInteractiveGuideline":true,"tooltips":true,"showLegend":true,"color":"colorFunction()","forcey":"[0,2]"}}]};
+
+    es.create({
+      index: index,
+      type: type,
+      id: storageId,
+      body: sampleMalharDashboard,
+      requestTimeout: 2000
+    }).then(function (body) {
+      console.log(body);
+    }, function (error) {
+      console.error(error.message);
+    });
+
+    es.get({
+      index: index,
+      type: type,
+      id: storageId,
+      requestTimeout: 2000
+    }).then(function (body) {
+      console.log(body);
+    }, function (error) {
+      console.error(error.message);
+    });
+
     var esStorage = null; // TODO
+
 
     $scope.dashboardOptions = {
 //      storage: esStorage,
-//      storageId: 'andy-dashboard-graphite-default-backend-storage',
+      storageId: storageId,
       widgetButtons: true,
       widgetDefinitions: WidgetDefaults.widgetDefaultDefinitions,
       defaultWidgets: WidgetDefaults.defaultWidgets
