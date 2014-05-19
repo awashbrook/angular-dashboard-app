52c48089d35a1084352a94e6a502abc2e090167d
awashbrook <awashbrook@gmail.com>
2014-05-19 12:54:26 +0100

    Dyamic routing of Dashboard ID now supported for ES

--- app/scripts/controllers/graphiteDefaulElasticStorage.js
+++ app/scripts/controllers/graphiteDefaulElasticStorage.js
@@ -2,23 +2,7 @@
 
 // Storage enabled for this...this will be what a fresh dashboard will look like for new team
 angular.module('app')
-  .controller('GraphiteDefaultElasticStorageCtrl', function ($scope, $interval, elasticStorage, nvd3ChartDefAttrs, WidgetDefaults) {
-
-    var index = 'malhar-dash';
-    var type = 'dashboard';
-    var storageId = 'andy-dashboard-graphite-default-backend-storage6';
-//    var sampleMalharDashboard = {"widgets":[{"title":"Mat","name":"nvLineChartAlpha","style":{"width":"400px"},"dataModelOptions":{"params":{"url":"http://metrics.alpha.eikon-mon.int.thomsonreuters.com/render/","from":"-2h","until":"now","target":["randomWalk(\"random walk 1\")","randomWalk(\"random walk 2\")","randomWalk(\"random walk 3\")"]}},"attrs":{"isArea":true,"height":400,"showXAxis":true,"showYAxis":true,"xAxisTickFormat":"xAxisTickFormat()","interactive":true,"useInteractiveGuideline":true,"tooltips":true,"showLegend":true,"color":"colorFunction()","forcey":"[0,2]"}},{"title":"Jan","name":"nvLineChartBeta","style":{"width":"400px"},"dataModelOptions":{"params":{"url":"http://metrics.beta.eikon-mon.int.thomsonreuters.com/render/","from":"-2h","until":"now","target":["randomWalk(\"andy random walk 1\")","randomWalk(\"olivier random walk 2\")","randomWalk(\"fabrice random walk 3\")"]}},"attrs":{"isArea":true,"height":400,"showXAxis":true,"showYAxis":true,"xAxisTickFormat":"xAxisTickFormat()","interactive":true,"useInteractiveGuideline":true,"tooltips":true,"showLegend":true,"color":"colorFunction()","forcey":"[0,2]"}},{"title":"Widget 3","name":"nvLineChartProd","style":{"width":"400px"},"dataModelOptions":{"params":{"url":"http://metrics.eikon-mon.int.thomsonreuters.com/render/","from":"-2h","until":"now","target":["randomWalk(\"random walk 1\")","randomWalk(\"random walk 2\")","randomWalk(\"random walk 3\")"]}},"attrs":{"isArea":true,"height":400,"showXAxis":true,"showYAxis":true,"xAxisTickFormat":"xAxisTickFormat()","interactive":true,"useInteractiveGuideline":true,"tooltips":true,"showLegend":true,"color":"colorFunction()","forcey":"[0,2]"}}]};
-
-    $scope.dashboardOptions = {
-      storage: elasticStorage,
-      storageId: storageId,
-      explicitSave: true,
-      widgetButtons: true,
-      widgetDefinitions: WidgetDefaults.widgetDefaultDefinitions,
-      defaultWidgets: WidgetDefaults.defaultWidgets
-      //AW Set custom widget template for graphite directive at dasboard level
-      // optionsTemplateUrl: 'scripts/widgets/graphite/graphite-options.tpl.html'
-    };
+  .controller('GraphiteDefaultElasticStorageCtrl', function ($scope, $interval, $route, $routeParams, elasticStorage, nvd3ChartDefAttrs, WidgetDefaults) {
 
     // Chart Options
     $scope.xAxisTickFormat = WidgetDefaults.xAxisTickFormat;
@@ -39,4 +23,31 @@ angular.module('app')
         }
       });
     };
+
+    $scope.$on('$routeChangeSuccess', function(event, routeData){
+//      http://deansofer.com/posts/view/14/AngularJs-Tips-and-Tricks-UPDATED#routing
+
+//      http://compass/#/dashboard/graphiteDefaultBackendStorage?id=<dashboard-storage-id>
+//
+//        $routeParams ==> {dashboard: graphiteDefaultBackendStorage,id:<dashboard-storage-id>}
+
+      var routeParams = routeData.params;
+//      console.log(routeParams);
+      var dynamicDashboardId = routeParams.id || 'graphite-default-elastic-storage';
+      console.log("Your dashboard id: " + dynamicDashboardId);
+
+      $scope.dashboardOptions = {
+        storage: elasticStorage,
+        storageId: dynamicDashboardId,
+        explicitSave: true,
+        widgetButtons: true,
+        widgetDefinitions: WidgetDefaults.widgetDefaultDefinitions,
+        defaultWidgets: WidgetDefaults.defaultWidgets
+        //AW Set custom widget template for graphite directive at dashboard level
+        // optionsTemplateUrl: 'scripts/widgets/graphite/graphite-options.tpl.html'
+      };
+
+
+    });
+
   });
